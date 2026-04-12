f7f650484ff68f49bd1e637f854df3a968cc0f86
Kaveesha Senarathne <160614811+Kaveeshamalindi@users.noreply.github.com>
2026-04-12 08:20:04 +0530

    Add files via upload

---
+++ Data Structures/Non-Linear Data Structures/Hash Table/DataItem.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package HashTable;
+import java.io.*;
+
+/**
+ *
+ * @author USER
+ */
+public class DataItem {
+    // (could have more data)
+    
+    private int iData;
+    
+    // constructor
+    public DataItem(int ii){
+        iData = ii;
+    }
+    
+    public int getKey(){
+         return iData;
+    }
+} // end class DataItem
